f2c340c4b155ffea4b5c67580a38ae4ee6af258c
Christine-design-web <wangecichristine39@gmail.com>
2023-02-16 18:24:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 /**
- * main - A c program that prints a line tomthe standard error
- * return:1 (success)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19".
+ * followed by a newline.
+ * Return: Always 1.
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
