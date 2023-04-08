fc1959acc0063557edf4be991945060b42a2711a
Christine-design-web <wangecichristine39@gmail.com>
2023-04-08 07:57:37 -0700

    I love coding

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * binary_to_uint - binary to uint
+ * @b: string to convert
+ * Return: The unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int p = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b == '1')
+			p = (p << 1) | 1;
+		else if (*b == '0')
+			p <<= 1;
+		else
+			return (0);
+		b++;
+	}
+	return (p);
+}
